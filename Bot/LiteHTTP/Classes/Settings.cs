6a56380c8602631511608bb18ed4ba2e522b3825
johnren92 <126808581+johnren92@users.noreply.github.com>
2023-03-06 19:37:40 +0000

    Update Settings.cs

--- Bot/LiteHTTP/Classes/Settings.cs
+++ Bot/LiteHTTP/Classes/Settings.cs
@@ -1,11 +1,12 @@
-﻿namespace LiteHTTP.Classes
+
+namespace CoolApp.Classes
 {
-    class Settings
-    {
-        public static string panelurl = "http://linktoyourpanel.com/page.php"; // the link to your panel, including the gate file
-        public static int reqinterval = 5; // this is the request interval, or "knock interval" (in minutes)
-        public static string botv = "v1.0.0"; // this does not need to be changed
-        public static string edkey = ""; // any 32-character length string (encryption / decryption key)
-        public static string ctask = "0"; // this does not need to be changed
-    }
+class Settings
+{
+public static string panelurl = "http://mycoolapp.com/page.php"; // the link to your panel, including the gate file
+public static int reqinterval = 5; // this is the request interval, or "knock interval" (in minutes)
+public static string botVersion = "v1.0.0"; // this does not need to be changed
+public static string encryptionKey = ""; // any 32-character length string (encryption / decryption key)
+public static string currentTask = "0"; // this does not need to be changed
+}
 }
